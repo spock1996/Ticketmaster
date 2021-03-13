75293dea8ebd2cebc093c04fe9774213a6df1172
Aleksandar Despotović <a.despotovic@obs.fin>
2021-03-13 23:05:20 +0100

    Setup for heroku

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
-const port = 3000;
-
-
+const port = process.env.PORT || 3000;
 // paths location
 const public = path.join(__dirname, "./public");
 const partials = path.join(__dirname, "./views/partials");
