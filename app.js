01542a697a6672b4ea62019f551fa0c4d5c23446
Aleksandar Despotović <a.despotovic@obs.fin>
2021-03-13 18:56:47 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const port = 3000;
+
+
+// paths location
+const public = path.join(__dirname, "./public");
+const partials = path.join(__dirname, "./views/partials");
+
+// call static set hbs and set partials
+app.use(express.static(public));
+app.set("view engine", "hbs");
+hbs.registerPartials(partials)
+
+
+// routing
+app.get("", (req, res) => {
+    res.render("main")
+})
+
+app.get("/requests", (req, res) => {
+    res.render("requests")
+})
+
+app.get("*", (req, res) => {
+    res.render("404")
+})
+
+// Start server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
